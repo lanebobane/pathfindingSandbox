8123f41969907e85c0bf7b9f7de1c12f60ad1671
Lane Zimmerman <lane.zimmerman@gmail.com>
2022-01-05 10:26:39 -0500

    basic functionality setup and README started

---
+++ README.md
@@ -0,0 +1,14 @@
+PathfindingSandbox.js
+==============
+#### A first look into Pathfinding.js, library I may use for my Capstone Project ####
+
+Introduction
+------------
+
+The aim of this project is to satisfy simple user-stories having to do with determining the shortest
+path to fetch a list of items.
+
+Demo
+------
+
+An [online demo](http://www.google.com) can be found to showcase the functionality.
--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 const PF = require('pathfinding');
 
-let grid = new PF.Grid(5, 7);
+var matrix = [
+  [0, 0, 1, 1],
+  [1, 0, 0, 0],
+  [0, 0, 1, 0],
+  [0, 0, 1, 0],
+];
+
+var gridMatrix = new PF.Grid(matrix);
+let grid = new PF.Grid(4, 4);
 
 var finder = new PF.AStarFinder();
 
-var path = finder.findPath(0, 0, 4, 6, grid);
+var path = finder.findPath(0, 0, 3, 3, grid);
+var pathMatrix = finder.findPath(0, 0, 3, 3, gridMatrix);
 
+console.log('no obstacles')
 console.log(path)
+console.log('obstacles')
+console.log(matrix)
+console.log("***")
+console.log(pathMatrix)
