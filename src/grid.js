05a995ed93d317817e0094f03728cdfd3c06b8c8
Lane Zimmerman <lane.zimmerman@gmail.com>
2022-01-05 11:32:44 -0500

    README done, ready to begin coding

---
+++ src/grid.js
@@ -0,0 +1,7 @@
+const grid1 = [
+  [{isWalkable: false, item: 0},{isWalkable: false, item: 0},{isWalkable: true, item: 0}],
+  [{isWalkable: true, item: 0},{isWalkable: true, item: "apple"},{isWalkable: true, item: 0}],
+  [{isWalkable: true, item: 0},{isWalkable: false, item: 0},{isWalkable: true, item: "banana"}]
+]
+
+module.exports = grid1;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,28 @@
 const PF = require('pathfinding');
+const grid1 = require('./grid.js')
 
-var matrix = [
-  [0, 0, 1, 1],
-  [1, 0, 0, 0],
-  [0, 0, 1, 0],
-  [0, 0, 1, 0],
-];
+// A quick demo of Pathfinding.js's functionality.
+function helloWorld() {
+  var matrix = [
+    [0, 0, 1, 1],
+    [1, 0, 0, 0],
+    [0, 0, 1, 0],
+    [0, 0, 1, 0],
+  ];
+  var gridMatrix = new PF.Grid(matrix);
+  let grid = new PF.Grid(4, 4);
 
-var gridMatrix = new PF.Grid(matrix);
-let grid = new PF.Grid(4, 4);
+  var finder = new PF.AStarFinder();
 
-var finder = new PF.AStarFinder();
+  var path = finder.findPath(0, 0, 3, 3, grid);
+  var pathMatrix = finder.findPath(0, 0, 3, 3, gridMatrix);
 
-var path = finder.findPath(0, 0, 3, 3, grid);
-var pathMatrix = finder.findPath(0, 0, 3, 3, gridMatrix);
+  console.log('no obstacles')
+  console.log(path)
+  console.log('obstacles')
+  console.log(matrix)
+  console.log("***")
+  console.log(pathMatrix)
+}
 
-console.log('no obstacles')
-console.log(path)
-console.log('obstacles')
-console.log(matrix)
-console.log("***")
-console.log(pathMatrix)
+console.log(grid1)
